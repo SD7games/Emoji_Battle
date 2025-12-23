d2ca2a5bf005af4dc2841ab418c3d2d9ae0060b9
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-12-23 15:52:13 +0200

    changed WinPopup text & added random text to the DefeatPopup.

--- Assets/Scripts/Presentation/Popups/Main/DefeatPopup.cs
+++ Assets/Scripts/Presentation/Popups/Main/DefeatPopup.cs
@@ -1,4 +1,34 @@
+using TMPro;
+using UnityEngine;
+
 public sealed class DefeatPopup : ResultPopup
 {
     public override PopupId Id => PopupId.Defeat;
+
+    [SerializeField] private TMP_Text _messageText;
+
+    private static readonly string[] DefeatTexts =
+    {
+        "Nice try!",
+        "Almost!",
+        "That was close!",
+        "Keep going!",
+        "Don't give up!",
+        "Not this time.",
+        "Try again.",
+        "The opponent was tough.",
+        "Ooops… not this time."
+    };
+
+    public override void Show()
+    {
+        SetRandomText();
+        base.Show();
+    }
+
+    private void SetRandomText()
+    {
+        int index = Random.Range(0, DefeatTexts.Length);
+        _messageText.text = DefeatTexts[index];
+    }
 }
